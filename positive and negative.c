b7122beb927e99a74a76ab60fcb9742333941081
arumugamskva <47448339+arumugamskva@users.noreply.github.com>
2019-02-08 17:06:41 +0530

    Create positive and negative.c

---
+++ positive and negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int s;
+	scanf("%d",&s);
+	if(s>0)
+	{
+		printf("Positive");
+	}
+	else if(s<0)
+	{
+		printf("Negative");
+	}
+	else
+	{
+		printf("Zero";)
+	}
+	return 0;
+}
